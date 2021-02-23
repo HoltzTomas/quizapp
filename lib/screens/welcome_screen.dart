cd937f6755afe257c2490a1aeee6bd2ede3c3664
Tomas Holtz <holtztomy@gmail.com>
2021-02-23 16:50:11 -0300

    Welcome Screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,8 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:websafe_svg/websafe_svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          //WebsafeSvg.asset("assets/icons/bg.svg", fit: BoxFit.fill),
+          SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Spacer(flex: 2,),
+                Text(
+                  "Let's Play Quiz",
+                  style: Theme.of(context).textTheme.headline4.copyWith(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Text("Enter your information below"),
+                Spacer(),
+                TextField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFF1C2341),
+                      hintText: "Full Name",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)))),
+                ),
+                Spacer(),
+                InkWell(
+                  onTap: () {
+                    print("HOLA");
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(kDefaultPadding * 0.75), //15
+                    decoration: BoxDecoration(
+                      gradient: kPrimaryGradient,
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                    ),
+                    child: Text(
+                      "Lets Start Quiz",
+                      style: Theme.of(context)
+                          .textTheme
+                          .button
+                          .copyWith(color: Colors.black),
+                    ),
+                  ),
+                ),
+                Spacer(flex: 2) //It will take 2/6
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
